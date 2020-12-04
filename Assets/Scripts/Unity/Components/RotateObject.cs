e123388289399a478ee53fd1b7e0f0c65d873d9a
AlexSerodio <alequis01@gmail.com>
2020-12-03 22:06:38 -0300

    add terrain rotation

---
+++ Assets/Scripts/Unity/Components/RotateObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float speed = 50f;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z+speed*Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z-speed*Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.LeftArrow))
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y+speed*Time.deltaTime, transform.eulerAngles.z);
+        
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y-speed*Time.deltaTime, transform.eulerAngles.z);
+
+        if(Input.GetKey(KeyCode.R))
+            transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+}
